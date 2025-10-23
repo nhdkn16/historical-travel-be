85b21e188939a26e1018a8350872de00a7863624
Nguyenhaidang45 <nguyenhaidang4524@gmail.com>
2025-10-23 23:13:00 +0700

    build: set up project

---
+++ src/main/java/com/nhdkn16/historicaltravel/HistoricaltravelApplication.java
@@ -0,0 +1,13 @@
+package com.nhdkn16.historicaltravel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HistoricaltravelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HistoricaltravelApplication.class, args);
+	}
+
+}
